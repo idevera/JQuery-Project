b09afadd05dc51fcd728b35aae668338d855590e
Irene <irenneeyyy@gmail.com>
2017-02-11 13:56:19 -0800

    Added toggle jQuery function to existing page

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
 $(function() {
-	$("h1").click(function() {
+	$("h1, h2").click(function() {
   	alert("This is a header.");
 	});
 
 	$("p").click(function() {
-	  alert("This is a paragraph.");
+	  alert("This is a paragraph.")
 	});
 
 	$("img").click(function() {
 	  alert("This is an image.");
+	});
+
+	$(".clickable").click(function() {
+	  $(".walrus-showing").toggle();
+	  $(".walrus-hidden").toggle();
 	});
 });
